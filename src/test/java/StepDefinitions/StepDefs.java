c3634b63775c120b2d29a4635bedd74857a286ef
Imran Khan <imran.4.khan@gm.com>
2024-06-23 05:04:44 +0530

    RestAPIFramework Updates

--- src/test/java/StepDefinitions/StepDefs.java
+++ src/test/java/StepDefinitions/StepDefs.java
@@ -4,8 +4,20 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
 
 public class StepDefs {
+    @Given("AddPlace Payload is available")
+    public void add_place_payload_is_available() {
+        RequestSpecification res=new RequestSpecBuilder()
+                .setBaseUri("https://rahulshettyacademy.com").setContentType(ContentType.JSON)
+                .addQueryParam("key","qaclick123").build();
+//        given().spec(res).when().post("");
+    }
     @When("user call {string} api using {string} http request")
     public void userCallApiUsingHttpRequest(String arg0, String arg1) {
     }
